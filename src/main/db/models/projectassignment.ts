069eba3f94593d55e808e52978219bb8572cddd2
Samuel Njenga <snjenga704@gmail.com>
2023-04-30 20:18:56 +0300

    feat: modify ProjectAssignment model and add id field

--- src/main/db/models/projectassignment.ts
+++ src/main/db/models/projectassignment.ts
@@ -1,15 +1,22 @@
 import {
+  AutoIncrement,
   Column,
   DataType,
   ForeignKey,
   Model,
-  Table,
+  PrimaryKey,
+  Table
 } from "sequelize-typescript";
 import { Project } from "./project";
 import { User } from "./user";
 
 @Table({ tableName: "projectAssignments" })
 export class ProjectAssignment extends Model<ProjectAssignment> {
+  @PrimaryKey
+  @AutoIncrement
+  @Column(DataType.INTEGER)
+  id!: number;
+
   @ForeignKey(() => User)
   @Column(DataType.INTEGER) // Specify the data type as INTEGER
   userId!: number;
